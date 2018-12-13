b03bd64ece6f7b2eae468d510c243708eac0e83d
Sharifah Munira <shifa@myorange.my>
2018-12-13 14:43:17 +0800

    blog change

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
+from .models import Blog
+
+def allblogs(request):
+    blogs = Blog.objects
+    return render(request, 'blog/allblogs.html', {'blogs':blogs})
+
 # Create your views here.
